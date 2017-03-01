65ee8c51d4014107e7547829aea167d9c797859e
Iva <iva.gornishka@student.uva.nl>
2017-03-01 13:15:39 +0100

    booboo

--- TerrorismFlask/app/settings.py
+++ TerrorismFlask/app/settings.py
@@ -8,5 +8,5 @@ class Config(object):
 
     DATA_PATH = 'app/static/data/globalterrorismdb_0616dist.csv'
 
-    COUNTRY_COUNTS = data.get_country_counts(DATA_PATH)
+#    COUNTRY_COUNTS = data.get_country_counts(DATA_PATH)
     GTD_DATA = data.GTDData(DATA_PATH)
