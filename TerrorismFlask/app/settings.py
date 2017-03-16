c329eeb2697a1e269360d53d4381ce0048ae5aca
jroeder <julius@roeder.cx>
2017-03-16 07:22:41 +0100

    commented out data path

--- TerrorismFlask/app/settings.py
+++ TerrorismFlask/app/settings.py
@@ -2,7 +2,7 @@
 
 class Config(object):
 
-    # DATA_PATH = 'app/static/data/globalterrorismdb_0616dist.csv'
+    DATA_PATH = 'app/static/data/globalterrorismdb_0616dist.csv'
     # For Helena's computer
-    DATA_PATH = '/mnt/0D8255B06E43F71B/MEGA/UVA/IV/UvA-IV2017-Team9/TerrorismFlask/app/static/data/globalterrorismdb_0616dist.csv'
-    #TODO: maybe add SERVER_NAME to run on the sesames at some point because.. why not
+    # DATA_PATH = '/mnt/0D8255B06E43F71B/MEGA/UVA/IV/UvA-IV2017-Team9/TerrorismFlask/app/static/data/globalterrorismdb_0616dist.csv'
+    # #TODO: maybe add SERVER_NAME to run on the sesames at some point because.. why not
