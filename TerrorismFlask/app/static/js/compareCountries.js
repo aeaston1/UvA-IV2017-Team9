f2239d0e78de6b457c8eec66e4c92752e93fcd80
Iva <iva.gornishka@student.uva.nl>
2017-03-29 17:42:17 +0200

    The zero elem for comparison

--- TerrorismFlask/app/static/js/compareCountries.js
+++ TerrorismFlask/app/static/js/compareCountries.js
@@ -1,7 +1,7 @@
 
 
 
-var plotCountryStuffs = function(countryData) {
+var plotCountryStuffs = function(countriesData) {
 
 /*
             countryData = current_attack_data;
@@ -10,6 +10,40 @@ var plotCountryStuffs = function(countryData) {
             if (previousCountry === selectedCountry) return previousCountry;
             previousCountry = selectedCountry;
 */
+
+
+        data = {{countryData|tojson}};
+//        draw_wordle(data1);
+//        console.log(data1.country);
+ 
+//        var data2 = {{country_data2|tojson}};
+//        draw_wordle(data2);
+
+//        var data = new Object()
+//        data[data1.country] = data1;
+//        data[data2.country] = data2;
+ 
+//      plot_radar(data, 'attacktype');
+
+        attack_radar_config = plot_radar(data, 'attacktype');
+        target_radar_config = plot_radar(data, 'targettype');
+        groups_radar_config = plot_radar(data, 'groups');
+        scatter_config = plot_scatter(data, 'attacks_per_period');
+
+        window.onload = function() {
+            var scatter_ctx = document.getElementById("scatter-attacks").getContext("2d");
+            window.myChart = new Chart(scatter_ctx, scatter_config)
+
+            var attack_radar_ctx = document.getElementById("radar-attacktype");
+            window.myPolarArea = Chart.PolarArea(attack_radar_ctx, attack_radar_config);
+
+            var groups_radar_ctx = document.getElementById("radar-groups");
+            window.myPolarArea = Chart.PolarArea(groups_radar_ctx, groups_radar_config);
+
+            var target_radar_ctx = document.getElementById("radar-targettype");
+            window.myPolarArea = Chart.PolarArea(target_radar_ctx, target_radar_config);
+        };
+
 
             var countryLabel = document.getElementById("countryName");
             countryLabel.innerHTML = countryData.country;
