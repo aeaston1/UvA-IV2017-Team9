b05cc0320f8d308bdda3ba1a800938a609251c6e
Iva <iva.gornishka@student.uva.nl>
2017-03-29 20:27:37 +0200

    Plotting compare countries! One at a time in a loop... 8D

--- TerrorismFlask/app/static/js/compareCountries.js
+++ TerrorismFlask/app/static/js/compareCountries.js
@@ -1,48 +1,7 @@
 
 
 
-var plotCountryStuffs = function(countriesData) {
-
-/*
-            countryData = current_attack_data;
-            console.log(selectedCountry, countryData, current_attack_data)
-            console.log("previous:"+ previousCountry);
-            if (previousCountry === selectedCountry) return previousCountry;
-            previousCountry = selectedCountry;
-*/
-
-
-        data = {{countryData|tojson}};
-//        draw_wordle(data1);
-//        console.log(data1.country);
- 
-//        var data2 = {{country_data2|tojson}};
-//        draw_wordle(data2);
-
-//        var data = new Object()
-//        data[data1.country] = data1;
-//        data[data2.country] = data2;
- 
-//      plot_radar(data, 'attacktype');
-
-        attack_radar_config = plot_radar(data, 'attacktype');
-        target_radar_config = plot_radar(data, 'targettype');
-        groups_radar_config = plot_radar(data, 'groups');
-        scatter_config = plot_scatter(data, 'attacks_per_period');
-
-        window.onload = function() {
-            var scatter_ctx = document.getElementById("scatter-attacks").getContext("2d");
-            window.myChart = new Chart(scatter_ctx, scatter_config)
-
-            var attack_radar_ctx = document.getElementById("radar-attacktype");
-            window.myPolarArea = Chart.PolarArea(attack_radar_ctx, attack_radar_config);
-
-            var groups_radar_ctx = document.getElementById("radar-groups");
-            window.myPolarArea = Chart.PolarArea(groups_radar_ctx, groups_radar_config);
-
-            var target_radar_ctx = document.getElementById("radar-targettype");
-            window.myPolarArea = Chart.PolarArea(target_radar_ctx, target_radar_config);
-        };
+var plotCompareCountryStuffs = function(countryData) {
 
 
             var countryLabel = document.getElementById("countryName");
@@ -59,16 +18,16 @@ var plotCountryStuffs = function(countriesData) {
             groups_radar_config = plot_radar(plotData, 'groups', false);
             scatter_config = plot_scatter(plotData, 'attacks_per_period');
 
-            var scatter_ctx = document.getElementById("scatter-attacks").getContext("2d");
+            var scatter_ctx = document.getElementById("scatter-compare-attacks").getContext("2d");
             window.myChart = new Chart(scatter_ctx, scatter_config)
 
-            var attack_radar_ctx = document.getElementById("radar-attacktype");
+            var attack_radar_ctx = document.getElementById("radar-compare-attacktype");
             window.myPolarArea = Chart.PolarArea(attack_radar_ctx, attack_radar_config);
 
-            var groups_radar_ctx = document.getElementById("radar-groups");
+            var groups_radar_ctx = document.getElementById("radar-compare-groups");
             window.myPolarArea = Chart.PolarArea(groups_radar_ctx, groups_radar_config);
 
-            var target_radar_ctx = document.getElementById("radar-targettype");
+            var target_radar_ctx = document.getElementById("radar-compare-targettype");
             window.myPolarArea = Chart.PolarArea(target_radar_ctx, target_radar_config);
 
 }
